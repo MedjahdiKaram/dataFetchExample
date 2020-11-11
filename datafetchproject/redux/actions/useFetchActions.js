54b8b36288ff153c4be101f4de400b93c9735555
Karam Medjahdi <karam.medjahdi@speechmobility.com>
2020-11-11 12:40:29 +0100

    Creation of the reducer and actions

---
+++ datafetchproject/redux/actions/useFetchActions.js
@@ -0,0 +1,26 @@
+export const FETCH_DATA_BEGIN = "FETCH_DATA_BEGIN";
+export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
+export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE";
+export const FETCH_DATA = "FETCH_DATA";
+
+export const fetchDataBegin = () => ({
+  type: FETCH_DATA_BEGIN,
+});
+
+export const fetchDataSuccess = (data) => ({
+  type: FETCH_DATA_SUCCESS,
+  payload: { data },
+});
+
+export const fetchDataFailure = (error) => ({
+  type: FETCH_DATA_FAILURE,
+  payload: { error },
+});
+
+// Handle HTTP errors thrown.
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
---
+++ datafetchproject/redux/reducers/useFetchReducers.js
@@ -0,0 +1,30 @@
+import {
+  FETCH_DATA_BEGIN,
+  FETCH_DATA_SUCCESS,
+} from "../actions/useFetchActions";
+
+const theState = { data: "" };
+export default function fetchDataReducer(state = theState, action) {
+  console.log("Action: ", action);
+  switch (action.type) {
+    case FETCH_DATA_SUCCESS:
+      localStorage.setItem(
+        "data",
+        JSON.stringify({ ...state.data, ...action.payload.data })
+      );
+      return {
+        data: { ...state.data, ...action.payload.data },
+        loading: false,
+      };
+
+    case FETCH_DATA_BEGIN:
+      const localStrageData = localStorage.getItem("data");
+      const init = localStrageData
+        ? { data: JSON.parse(localStrageData), loading: true, error: null }
+        : theState;
+      return init;
+
+    default:
+      return state;
+  }
+}
